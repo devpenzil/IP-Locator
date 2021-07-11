df0394dfda3af1aba77f41f8d8c8ecb2870e7d19
ajo <ajoalex012@gmail.com>
2021-07-11 21:10:26 +0530

    map is working

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,10 +1,22 @@
-import React, { useEffect, useState } from 'react'
+import React, {useRef, useEffect, useState } from 'react'
 import axios from 'axios'
 import right from '../../src/Assets/images/icon-arrow.svg'
 import './Home.css'
+import mapboxgl from 'mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
+ 
+
+
 function Home() {
+    mapboxgl.accessToken = 'pk.eyJ1IjoiYWpvYWxleDAxMiIsImEiOiJja3F5eDA3YjYxYzhsMnZvMWFyNGpodDJvIn0.1fs-cBrslN2Is_IBzlwhyg';
+    const mapContainer = useRef(null);
+    const map = useRef(null);
+    const [lng, setLng] = useState(-70.9);
+    const [lat, setLat] = useState(42.35);
+    const [zoom, setZoom] = useState(9);
+    //----------------------
     const [ipdata, setIpdata] = useState()
     const [userip, setUserip] = useState("157.44.186.61")
+    //--------------------------
     const ipcalled = () =>{
         axios.get(`https://geo.ipify.org/api/v1?apiKey=at_yFvUMou2NIuvOi6o0QBY0cpo3ntOp&ipAddress=${userip}`).then((Response)=>{
            // console.log(Response.data)
@@ -14,13 +26,23 @@ function Home() {
             alert(error.message)
         })
     }
+    //----------------------
 
     useEffect(() => {
         ipcalled()
-    }, [])
+        if (map.current) return; // initialize map only once
+        map.current = new mapboxgl.Map({
+        container: mapContainer.current,
+        style: 'mapbox://styles/mapbox/streets-v11',
+        center: [lng, lat],
+        zoom: zoom
+        });
+        });
+    //--------------------
     const clicked = () =>{
         ipcalled()
     }
+    //-------------------
 
     return (
         
@@ -56,7 +78,8 @@ function Home() {
                </div>
             </div>
             <div className="bottom">
-            
+
+            <div ref={mapContainer} className="map-container" />
             </div>
         </div>
     )
